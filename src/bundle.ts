0e1ba6589beccb4c7ae0d58fc4e39a6b101e7169
valleykid <valleykiddy@gmail.com>
2022-03-28 17:11:52 +0800

    feat: support h5

--- src/bundle.ts
+++ src/bundle.ts
@@ -6,18 +6,23 @@ registerSubapps([
     name: 'seeconf',
     entry: 'https://seeconf2019.antfin.com/',
     rules: [{ rule: '/seeconf' }],
-    rootVars: { routerBase: '/seeconf' },
+    rootVars: { routerBase: '/seeconf', endType: 'none' },
   },
   {
     name: 'juejin',
     entry: 'https://juejin.cn/',
-    rules: [{ rule: '/juejin', layout: 'seeconf > /seeconf', container: '#guests' }],
+    rules: [{ rule: '/juejin', layout: 'seeconf > /seeconf', container: '#guests', endType: 'none' }],
     ignoreFileRules: ['analytics.js'],
   },
   {
     name: 'koubei',
     entry: 'https://www.koubei.com/',
     rules: [{ rule: '/' }],
+  },
+  {
+    name: 'koubei_m',
+    entry: 'https://render.koubei.com/p/f/koubei/index.html?appCh=koubei_m',
+    rules: [{ rule: '/', endType: 'h5' }],
   },
 ]);
 
